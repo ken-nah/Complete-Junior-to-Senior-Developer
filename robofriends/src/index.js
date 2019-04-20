05cb4511d8b6eaa4f466966f43f98acee64a879c
ken-nah <Kennyadduz@gmail.com>
2019-04-20 12:41:31 +0300

    A small robot application

---
+++ robofriends/src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Card from "./card";
+import * as serviceWorker from "./serviceWorker";
+import "tachyons";
+
+import { robots } from "./robots";
+
+ReactDOM.render(
+  <Card
+    name={robots[0].name}
+    email={robots[0].email}
+    id={robots[0].id}
+  />,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
